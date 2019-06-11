df147cc1c18e52823b254acb4fa11768e4969223
Kevin Veldkamp <kveldkamp@veyo.com>
2019-06-10 23:15:17 -0700

    Initial Commit

---
+++ onTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  onTheMap
+//
+//  Created by Kevin Veldkamp on 6/10/19.
+//  Copyright © 2019 kevin veldkamp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
