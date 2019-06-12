48538708b5ffa79f82cb825851b524bc24549bbb
Kevin Veldkamp <kveldkamp@veyo.com>
2019-06-11 19:05:50 -0700

    linked UI elements

---
+++ onTheMap/LoginVC.swift
@@ -0,0 +1,29 @@
+//
+//  LoginVC.swift
+//  onTheMap
+//
+//  Created by Kevin Veldkamp on 6/10/19.
+//  Copyright © 2019 kevin veldkamp. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    @IBOutlet weak var usernameTextfield: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+    }
+    
+
+}
+
