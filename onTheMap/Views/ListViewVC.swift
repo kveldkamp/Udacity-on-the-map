493d8e2252e464dd8bd1c8dc2e4a048a540b278c
Kevin Veldkamp <kveldkamp@veyo.com>
2019-06-16 14:57:29 -0700

    linked all UI elements on map and list view

---
+++ onTheMap/Views/ListViewVC.swift
@@ -0,0 +1,29 @@
+//
+//  ListViewVC.swift
+//  onTheMap
+//
+//  Created by Kevin Veldkamp on 6/16/19.
+//  Copyright © 2019 kevin veldkamp. All rights reserved.
+//
+
+import UIKit
+
+class ListViewVC: UIViewController {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    @IBAction func logout(_ sender: Any) {
+    }
+    
+    
+    @IBAction func refresh(_ sender: Any) {
+    }
+    
+    
+    @IBAction func addPin(_ sender: Any) {
+    }
+    
+}
---
+++ onTheMap/Views/MapViewVC.swift
@@ -0,0 +1,28 @@
+//
+//  MapViewVC.swift
+//  onTheMap
+//
+//  Created by Kevin Veldkamp on 6/16/19.
+//  Copyright © 2019 kevin veldkamp. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewVC: UIViewController {
+    
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    
+    @IBAction func logout(_ sender: Any) {
+    }
+    
+    @IBAction func refresh(_ sender: Any) {
+    }
+    
+    @IBAction func addPin(_ sender: Any) {
+    }
+    
+}
